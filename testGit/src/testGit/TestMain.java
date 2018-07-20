727b27c99dac85f554a6e10e13e685008eee1bcc
lyy <120553590@qq.com>
2018-07-20 11:14:10 +0800

    测试3

--- testGit/src/testGit/TestMain.java
+++ testGit/src/testGit/TestMain.java
@@ -4,7 +4,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("helloword sheji moshi1");
+		System.out.print("helloword sheji moshi2");
 	}
 
 }
