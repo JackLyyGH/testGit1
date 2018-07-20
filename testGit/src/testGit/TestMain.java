8ede6037122b31d1303d7c0c6942cd8b6151c048
lyy <120553590@qq.com>
2018-07-20 12:22:46 +0800

    1231123

--- testGit/src/testGit/TestMain.java
+++ testGit/src/testGit/TestMain.java
@@ -4,7 +4,10 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("helloword sheji moshi4");
+		System.out.print("helloword sheji moshi4");	
+		
+		Shua1 shua1 = new Helloword1();
+		shua1.shua1print();
 	}
 
 }
