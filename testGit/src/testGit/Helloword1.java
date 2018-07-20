ddf829d3675f169680d768c7d926d528ea21a3a4
lyy <120553590@qq.com>
2018-07-20 12:21:38 +0800

    insert profiles

---
+++ testGit/src/testGit/Helloword1.java
@@ -0,0 +1,11 @@
+package testGit;
+
+public class Helloword1 implements Shua1{
+
+	@Override
+	public void shua1print() {
+		// TODO Auto-generated method stub
+		System.out.print("ceshi1");
+	}
+	
+}
---
+++ testGit/src/testGit/Shua1.java
@@ -0,0 +1,5 @@
+package testGit;
+
+public interface Shua1 {
+	void shua1print();
+}
